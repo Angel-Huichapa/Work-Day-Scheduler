9cbc210be7d4de8da8126864b2b79af309a81e76
Angel-Huichapa <146030239+Angel-Huichapa@users.noreply.github.com>
2024-04-09 03:58:17 -0600

    Added code to apply the past, present, or future class to each time block and I added code to display the current date in the header of the page.

--- Assets/script.js
+++ Assets/script.js
@@ -12,16 +12,23 @@ $(function () {
       var userInput = saveButton.prev().val(); // Assuming user input is in the input field preceding the save button
       localStorage.setItem(timeBlockId, userInput);
   });
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
+    // Add code to apply the past, present, or future class to each time block
+    $('.time-block').each(function() {
+      var timeBlock = $(this);
+      var currentHour = parseInt(dayjs().format('H')); // Get current hour in 24-hour time
+      var timeBlockHour = parseInt(timeBlock.attr('id').replace('hour-', ''));
+      if (timeBlockHour < currentHour) {
+          timeBlock.addClass('past');
+      } else if (timeBlockHour === currentHour) {
+          timeBlock.addClass('present');
+      } else {
+          timeBlock.addClass('future');
+      }
+  });
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
+    // Add code to display the current date in the header of the page
+    $('#currentDay').text(dayjs().format('dddd, MMMM D'));
 });
